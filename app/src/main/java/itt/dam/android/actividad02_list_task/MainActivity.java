93476c6e7a3faf777874640e8903421a0af94e28
japc <japc.grafico@gmail.com>
2020-01-12 20:55:37 +0100

    Dialog Add Task

--- app/src/main/java/itt/dam/android/actividad02_list_task/MainActivity.java
+++ app/src/main/java/itt/dam/android/actividad02_list_task/MainActivity.java
@@ -1,38 +1,57 @@
 package itt.dam.android.actividad02_list_task;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.customToolBar);
+        // Se añade el toolbar y menu personalizado.
+        Toolbar toolbar = findViewById(R.id.customToolBar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         toolbar.inflateMenu(R.menu.menu_bar);
     }
 
+    // Para añadir el menu al toolbar.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_bar, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
+    // Para añadirle funcionalidad a los items del menu personalizado.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        Toast.makeText(this, "Prueba", Toast.LENGTH_SHORT).show();
+
+        EditText inputText= new EditText(this);
+
+        AlertDialog writeTask = new AlertDialog.Builder(this)
+                .setTitle("Nueva tarea")
+                .setView(inputText)
+                .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancelar", null)
+                .create();
+        writeTask.show();
         return super.onOptionsItemSelected(item);
     }
 }
