c254f66b87e33a6656de484386711563fbbe9c36
japc <japc.grafico@gmail.com>
2020-01-13 10:45:31 +0100

    TaskList y DB connection

--- app/src/main/java/itt/dam/android/actividad02_list_task/MainActivity.java
+++ app/src/main/java/itt/dam/android/actividad02_list_task/MainActivity.java
@@ -8,22 +8,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ListView;
+
 import androidx.appcompat.widget.Toolbar;
 
+import itt.dam.android.actividad02_list_task.db.DbControler;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DbControler dbControler;
+    private ArrayAdapter<String> arrayAdapter;
+    private ListView listViewTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dbControler = new DbControler(this);
+        listViewTasks = (ListView) findViewById(R.id.listTask);
+
         // Se añade el toolbar y menu personalizado.
         Toolbar toolbar = findViewById(R.id.customToolBar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         toolbar.inflateMenu(R.menu.menu_bar);
+
+        updateUI();
     }
 
     // Para añadir el menu al toolbar.
@@ -38,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
 
-        EditText inputText= new EditText(this);
+        final EditText inputText= new EditText(this);
 
         AlertDialog writeTask = new AlertDialog.Builder(this)
                 .setTitle("Nueva tarea")
@@ -46,12 +59,22 @@ public class MainActivity extends AppCompatActivity {
                 .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-
+                        dbControler.addTask(inputText.getText().toString());
+                        updateUI();
                     }
                 })
                 .setNegativeButton("Cancelar", null)
                 .create();
         writeTask.show();
         return super.onOptionsItemSelected(item);
+    }
+
+    private void updateUI() {
+        if(dbControler.isEmptyDb()) {
+            listViewTasks.setAdapter(null);
+        } else {
+            arrayAdapter = new ArrayAdapter<String>(this, R.layout.item_todolist, R.id.taskTxt, dbControler.getTasks());
+            listViewTasks.setAdapter(arrayAdapter);
+        }
     }
 }
---
+++ app/src/main/java/itt/dam/android/actividad02_list_task/db/DbControler.java
@@ -0,0 +1,89 @@
+package itt.dam.android.actividad02_list_task.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DbControler extends SQLiteOpenHelper {
+    public DbControler(@Nullable Context context) {
+        super(context, "itt.dam.android.actividad02_list_task.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.i("App", "Se crea la bd");
+        db.execSQL("CREATE TABLE TASKS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void addTask(String s) {
+        Log.i("App", "Pasa por addTask");
+        ContentValues r = new ContentValues();
+        r.put("NOMBRE", s);
+
+
+        // Se abre la BD para escritura
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Se añade el registra  a la tarea
+        db.insert("TASKS", null, r);
+
+
+        // Se cierra la BD
+        db.close();
+    }
+
+    public String[] getTasks() {
+        Log.i("App", "Pasa por getTasks");
+        // Se abre la BD para lectura
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Se añade el registra  a la tarea
+        Cursor cursor = db.rawQuery("SELECT * FROM TASKS", null);
+
+        if (cursor.getCount() > 0){
+            String[] tasks = new String[cursor.getCount()];
+
+            cursor.moveToFirst();
+
+            for (int i = 0; i < cursor.getCount(); i++) {
+                tasks[i] = cursor.getString(1);
+                cursor.moveToNext();
+            }
+
+            db.close();
+            return tasks;
+
+        } else {
+            db.close();
+            return null;
+        }
+
+    }
+
+    public boolean isEmptyDb() {
+        Log.i("App", "Pasa por isEmpyDb");
+        // Se abre la BD para lectura
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        // Se añade el registra  a la tarea
+        Cursor cursor = db.rawQuery("SELECT * FROM TASKS", null);
+
+        if (cursor.getCount() > 0){
+            Log.i("APP", "Version Click:" + cursor.getCount());
+            return false;
+        } else {
+            Log.i("App", "BD vacia");
+            return true;
+        }
+    }
+}
